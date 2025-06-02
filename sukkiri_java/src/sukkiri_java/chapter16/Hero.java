42409775ab0330781b933329c9ef6e79e07153c4
ran-huang <huangran.alex@gmail.com>
2025-06-02 20:07:17 +0900

    add practice 16

---
+++ sukkiri_java/src/sukkiri_java/chapter16/Hero.java
@@ -0,0 +1,7 @@
+package sukkiri_java.chapter16;
+
+public class Hero {
+	private String name;
+	public Hero(String name) { this.name = name;}
+	public String getName() { return this.name;};
+}
---
+++ sukkiri_java/src/sukkiri_java/chapter16/Practice16.java
@@ -0,0 +1,37 @@
+package sukkiri_java.chapter16;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice16 {
+	public static void main(String[] args) {
+//		practice16_2();
+		practice16_3();
+	}
+	
+	public static void practice16_2() {
+		Hero saito = new Hero("Saito");
+		Hero suzuki = new Hero("Suzuki");
+		ArrayList<Hero> list = new ArrayList<>();
+		list.add(saito);
+		list.add(suzuki);
+		
+		for (Hero hero : list) {
+			System.out.println(hero.getName());
+		}
+	}
+	
+	public static void practice16_3() {
+		Hero saito = new Hero("Saito");
+		Hero suzuki = new Hero("Suzuki");
+		
+		Map<Hero, Integer> map = new HashMap<>();
+		map.put(saito, 3);
+		map.put(suzuki, 7);
+		
+		for(Hero h : map.keySet()) {
+			System.out.printf("%s beats %d enemies.\n", h.getName(), map.get(h));
+		}
+	}
+}
